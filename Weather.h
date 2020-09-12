3bfdf1ddce5c6f35ba022e692fca5001bacea2dc
Carlos Castro García <alu0100819847@ull.edu.es>
2020-09-12 19:15:21 +0100

    Structure Weather added #3

---
+++ Weather.h
@@ -0,0 +1,16 @@
+
+typedef struct {
+	char fecha[12];
+	char ciudad[40];
+	float temperaturaMaxima;
+	float temperaturaMinima;
+	float precipitacion;
+	float nubosidad;
+}Weather;
+
+
+Weather createTiempo(char inputData[]);
+
+void printWeather(Weather weather);
+
+float celciusToFarenheit(float celcius);
--- main.c
+++ main.c
@@ -1,21 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 #include<stdbool.h>
- 
+
+#include<string.h>
+#include"Weather.h"
  
 int main(int argc, char *argv[]){
 	FILE *file;
 	int c;
 	char uri[] = "data/Meteologica_vacante_ProgramadorC_20200901_datos.csv";
 	char input[200];
+	Weather weather;
 	file = fopen(uri, "rt");
 	bool header = true;
 	char counter = 0;
 	while((c=fgetc(file))!=EOF){
 		if(c=='\n'){
 			if(!header){
-				printf("\n%s", input);
+			printf("\n%s", input);
 			}
 			header = false;
 			memset(input,0,200);
